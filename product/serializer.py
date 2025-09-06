72bfb402cb20a67f100920bfcca6556c08bbe5fb
Tazul Islam <linkedin52425@gmail.com>
2025-09-06 15:54:01 +0600

    learn serialize instance save and deserialize

--- product/serializer.py
+++ product/serializer.py
@@ -8,7 +8,7 @@ from product.models import Product, Category
 #     description = serializers.CharField()
 
 class CategorySerializer(serializers.ModelSerializer):
-    product_count = serializers.IntegerField()
+    product_count = serializers.IntegerField(read_only=True)
     class Meta:
         model = Category
         fields = ['id', 'name', 'description', 'product_count']
@@ -32,12 +32,18 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = ['id', 'name', 'description', 'stock', 'price', 'category', 'tax_price']
 
-    category = serializers.HyperlinkedRelatedField(
-         queryset = Category.objects.all(),
-         view_name = 'view_specific_category',
-     )
+    # category = serializers.HyperlinkedRelatedField(
+    #      queryset = Category.objects.all(),
+    #      view_name = 'view_specific_category',
+    #  )
 
     tax_price = serializers.SerializerMethodField(method_name='calculate_tax')
 
     def calculate_tax(self, product):
          return round(product.price * Decimal(1.1), 2)
+
+    # field validators
+    def validate_price(self, price):
+        if price <= 0:
+            raise serializers.ValidationError("Price could not negative")
+        return price
--- product/views.py
+++ product/views.py
@@ -8,26 +8,53 @@ from django.shortcuts import get_object_or_404
 from product.serializer import CategorySerializer
 from django.db.models import Count
 
-@api_view()
+@api_view(['GEt','POST'])
 def view_products(request):
-    products = Product.objects.select_related('category').all()
-    serializer = ProductSerializer(products, many=True, context={'request':request})
-    return Response(serializer.data)
-
-@api_view()
-def view_specific_product(request, pk):
-    try:
-        p = Product.objects.get(pk=pk)
-        serializer = ProductSerializer(p)
+    if request.method == 'GET':
+        products = Product.objects.select_related('category').all()
+        serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
-    except Product.DoesNotExist:
-        return Response({"message":"Product does not exists"}, status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            print(serializer.validated_data)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+    
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
+def view_specific_product(request, pk):
+    if request.method == 'GET':
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+       product = get_object_or_404(Product, pk=pk)
+       serializer = ProductSerializer(product, data=request.data)
+       serializer.is_valid(raise_exception=True)
+       serializer.save()
+       return Response(serializer.data)
+    if request.method == 'DELETE':
+        product = get_object_or_404(Product, pk=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)     
+
+@api_view(['GET', 'POST'])
 def category_view(request):
-    category = Category.objects.annotate(product_count = Count('product')).all()
-    serializer = CategorySerializer(category, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        category = Category.objects.annotate(product_count = Count('product')).all()
+        serializer = CategorySerializer(category, many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+    
 
 @api_view()
 def specific_category_view(request, pk):
